1af488a85c45d15e5eabe9d6496234439d99d23a
Matteo Vellere <matteo.vellere@salvagninigroup.com>
2019-11-15 15:12:10 +0100

    inizio

--- GOL_1/GOL_1/UnitTest1.cs
+++ GOL_1/GOL_1/UnitTest1.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GOL_1
 {
+    public class Grid
+    {
+        public int Width { get; set; }
+        public int Heigth { get; set; }
+
+        public Cell GetCell(Row x, int y)
+        {
+
+        }
+    }
+
+    public class Row
+    {
+        public List<Cell> cells; 
+    }
+
+    public class Cell
+    {
+        public bool CurrState { get; set; }
+        public bool OldState { get; set; }
+
+        public bool Evolve()
+        {
+            bool newState = true;
+            return newState;
+        }
+    }
+
     [TestClass]
     public class UnitTest1
     {
